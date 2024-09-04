430a1d4b0ea1b0334dfb4982ce8e556334413e7a
adl.fahmia <adl.fahmia@xl.co.id>
2024-09-04 22:36:58 +0700

    status bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
         />
+        <meta name="theme-color" content="#10001b" />
       </head>
       <GlobalProvider>
         <body>{children}</body>
